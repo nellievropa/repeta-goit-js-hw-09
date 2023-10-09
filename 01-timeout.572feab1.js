d4074d9b205a4e2d4ee833138144769fc73c757b
nellievropa <124687833+nellievropa@users.noreply.github.com>
2023-10-09 13:50:52 +0000

    Deploying to gh-pages from @ nellievropa/repeta-goit-js-hw-09@86229b7b528de2adc98ffa1933227c03f1d3783d 🚀

---
+++ 01-timeout.572feab1.js
@@ -0,0 +1,2 @@
+!function(){console.log("До визову setTimeout"),setTimeout((function(){console.log("Внутри caalback-функції для setTimeout")}),2e3),console.log("Після визову setTimeout");for(var o=0;o<1e4;o++)console.log(o)}();
+//# sourceMappingURL=01-timeout.572feab1.js.map
