697dcafc8aeb03eb71e5e877be6f4577c78293e1
nellievropa <nellievropa23@gmail.com>
2023-10-19 16:59:47 +0300

    timer start

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,1 +1,12 @@
 
+
+
+
+
+function getTimeComponents(time) {
+    const hours = pad(Math.floor((time % (1000 * 60 * 60 *24)) / (1000 * 60 * 60)));
+    const mins = pad( Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+            return { hours, mins, secs};
+}
