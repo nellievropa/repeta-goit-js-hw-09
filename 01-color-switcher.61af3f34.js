8eab0065abd2da24a6d0d398652d25ab46248004
nellievropa <124687833+nellievropa@users.noreply.github.com>
2023-10-20 07:56:22 +0000

    Deploying to gh-pages from @ nellievropa/repeta-goit-js-hw-09@2f01838c2efd207bdccda919d75dc424976f7c8e 🚀

--- 01-color-switcher.61af3f34.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button");function n(){setInterval((function(){console.log("Змінюємо колір!"),document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)}t.addEventListener("data-start",n),t.addEventListener("data-stop",(function(){})),n()}();
-//# sourceMappingURL=01-color-switcher.61af3f34.js.map
--- 01-color-switcher.ed97d9ed.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button");function n(){setInterval((()=>{console.log("Змінюємо колір!"),document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)}t.addEventListener("data-start",n),t.addEventListener("data-stop",(function(){})),n();
-//# sourceMappingURL=01-color-switcher.ed97d9ed.js.map
