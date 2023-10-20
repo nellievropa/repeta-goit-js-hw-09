2f01838c2efd207bdccda919d75dc424976f7c8e
nellievropa <nellievropa23@gmail.com>
2023-10-20 10:55:59 +0300

    color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,31 +1,43 @@
+import throttle from "lodash.throttle";
+
+const CHANGE_COLOR = 2000;
+
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]')
 
 
-const CHANGE_COLOR = 1000;
+startBtn.addEventListener('click', throttle(onStart, 500));
+stopBtn.addEventListener('click', onStop);
 
-const button = document.querySelector('button');
+// const PROMT_DELAY = 1000;
 
-button.addEventListener('data-start', onStart);
-button.addEventListener('data-stop', onStop);
-
-
-
-function onStart() {
-
-    setInterval(() => {
+function onStart(evt) {
+  if (!evt) {
+    return;
+  }
         console.log('Змінюємо колір!');
-        document.body.style.background = getRandomHexColor();
-            }, CHANGE_COLOR);
+        setInterval(changeColors,CHANGE_COLOR);
+ }
     
+// // changeColors();
+// setInterval(changeColors,CHANGE_COLOR);
+// 
 
+function onStop(evt) {
+    const timerId = setInterval(changeColors,CHANGE_COLOR);
+    if(evt){
+        console.log('Зупиняємо інтервал!');
+        clearInterval(timerId);
+    }
+             
 
 }
 
-onStart();
-
-
-function onStop() {
-
+function changeColors() {
+    console.log('Змінюємо колір!');
+    document.body.style.background = getRandomHexColor();
 }
+
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
   }
