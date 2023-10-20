35a045f6d3708e98ff97dcecb63e1533f143cd5d
nellievropa <nellievropa23@gmail.com>
2023-10-20 15:00:34 +0300

    timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,12 +1,71 @@
 
 
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    datePicker: document.querySelector('#datetime-picker'),
+    daysValue: document.querySelector('span[data-days]'),
+    hoursValue: document.querySelector('span[data-hours]'),
+    minsValue: document.querySelector('span[data-minutes]'),
+    secsValue: document.querySelector('span[data-seconds]'),
+}
+
+const startTime = chooseDate();
+
+const timer = {
+    intervalId: null,
+    isActive: false,
+    start() {
+        if(isActive) {
+            return;
+        }
+
+        this.isActive = true;
+
+this.intervalId = setInterval(() => {
+    const currentTime = Date.now();
+    const dateTime = startTime- currentTime;
+    // const { days, hours, mins, secs } = getTimeComponents(deltaTime);
+    // деструктуризацію змінюємо на змінну time
+    // const time = getTimeComponents(deltaTime);
+    // і викликаємо функцію updateClockFace
+    // updateClockFace(time);
+    
+    console.log(`${days}:${hours}:${mins}:${secs}`);
+}, 1000);
+    },
+stop() {
+    clearInterval(this.intervalId);
+},
+
+};
+
+refs.startBtn.addEventListener('click', () => {
+    timer.start();
+})
 
 
+refs.stopBtn.addEventListener('click', () => {
+    timer.stop();
+})
+
+
+
+function updateClockFace ({days, hours, mins, secs}) {
+    requestAnimationFrame.clockface.textContent = `${days}:${hours}:${mins}:${secs}`;
+}
+
+//  для того, щоб зробити числа завжди двозначними (01 або 25) використовуємо метод pad
+// ми в нього обернемо наші значення hours, mins, secs
+function pad(value) {
+    // цей запис означає : візьми число, зроби з нього строку і на старті(ЗЛІВА!!) додай '0'
+    return String(value).padStart(2, '0');
+}
 
 function getTimeComponents(time) {
-    const hours = pad(Math.floor((time % (1000 * 60 * 60 *24)) / (1000 * 60 * 60)));
+    const days = pad(Math.floor((time % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24)));
+    const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
     const mins = pad( Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
         const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
 
-            return { hours, mins, secs};
+            return { days, hours, mins, secs};
 }
--- src/js/06-timer.js
+++ src/js/06-timer.js
@@ -1,10 +1,11 @@
 import '../css/common.css';
+// import '../css/01-color-switcher.css';
 
 // пишемо интерфейс секундоміра
-const refs ={
+const refs = {
     startBtn: document.querySelector('button[data-action-start]'),
     stopBtn: document.querySelector('button[data-action-stop]'),
-    clockface: document.querySelector('.js-clockface'),
+    clockface: document.querySelector('.js-clockface')
 };
 
 
@@ -14,17 +15,30 @@ const timer = {
     // щоб відстежувати кнопку старт, що вона активн
     isActive: false,
 start() {
+    // щоб зробити кнопку неактивною під час виконання коду
+    // перевіряємо її і якщо- да то перериваємо виконання коду подальшого
+    if (this.isActive) {
+        return;
+    }
     // в локальну зміну помістимо час, який є на момент старту
     const startTime = Date.now();
+
+    // а в цій чаті коду пишемо,що вона активна
+    this.isActive = true;
 
     // і запускаємо інтервал, який буде викликати цю функцію кожну секунду(або інший заданий проміжок часу)
     this.intervalId = setInterval(() => {
         // створимо стартовий час на момент запуску - він буде різний кожного разу!
         const currentTime = Date.now();
         // створимо змінну з різницею цих часів
-        const deltaTime = currentTime -startTime;
+        const deltaTime = currentTime - startTime;
         // cтворимо змінну, яка буде переводити мілісекунди в потрібний формат за допомогою функції
-        const {hours, mins, secs} = getTimeComponents(deltaTime)
+        const time = getTimeComponents(deltaTime); 
+    // деструктуризацію змінюємо на змінну time
+    // const times = getTimeComponents(deltaTime);
+
+     updateClockface(time);
+
         // console.log('start-> currentTime', currentTime)
         // час старту startTime буде незмінний!
         // console.log('start => startTime', startTime);
@@ -32,7 +46,7 @@ start() {
         // знайдемо різницю між часом старту та поточним часом
         // console.log( currentTime - startTime);
         // отримали наш рахівник секунд
-        console.log(`${hours}:${mins}:${secs}`);
+        // console.log(`${hours}:${mins}:${secs}`);
 
         // при використанні цього формату максимальна кількість годин- 24, а якщо на распродажу відведено більше годи-- то це вже проблема при використанні таких форматів часу
 // console.log(`${pad(new Date(deltaTime).getUTCHours())}:${pad(new Date(deltaTime).getMinutes())}:${pad(new Date(deltaTime).getSeconds())}`);
@@ -42,6 +56,8 @@ start() {
 // щоб зупинити секундомер використовуємо метод clearInterval для якого потрібен ідентифікатор ID інтервала, який визваний в другому методі старт!
 stop () {
     clearInterval(this.intervalId);
+    // тут знову кажемо, що кнопка неактивна повинна бути
+    this.isActive = false;
 }
 }
 
@@ -54,15 +70,21 @@ refs.startBtn.addEventListener('click', () => {
 refs.stopBtn.addEventListener('click', () => {
     timer.stop();
 })
-function updateClockFace ({hours, mins, secs}) {
-    requestAnimationFrame.clockface.textContent = `${hours}:${mins}:${secs}`;
+
+// малюємо ІНТЕРФЕЙС!! 
+// приймає мілісекунди
+// вираховує скільки в них годин, минут і секунд
+// малює інтерфейс
+
+function updateClockface ( { hours, mins, secs } ) {
+    refs.clockface.textContent = `${hours}:${mins}:${secs}`;
 }
 
 //  для того, щоб зробити числа завжди двозначними (01 або 25) використовуємо метод pad
 // ми в нього обернемо наші значення hours, mins, secs
 function pad(value) {
     // цей запис означає : візьми число, зроби з нього строку і на старті(ЗЛІВА!!) додай '0'
-    return String(value).padStart(2, '0')
+    return String(value).padStart(2, '0');
 }
 
 // Для того щоб перевести мілісекунди в поточний час є функція, якф
@@ -76,6 +98,6 @@ function getTimeComponents(time) {
     const mins = pad( Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
         const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
 
-            return { hours, mins, secs};
+            return { hours, mins, secs };
 }
 
