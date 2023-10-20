759e7314baa96e7fd5991056c5afe840c8a8918a
nellievropa <nellievropa23@gmail.com>
2023-10-20 11:57:43 +0300

    colorpicker

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,35 +1,33 @@
 import throttle from "lodash.throttle";
 
-const CHANGE_COLOR = 2000;
+const CHANGE_COLOR = 500;
 
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]')
 
+let TIMER_Id = null;
+// let startBtn.disabled = false;
 
 startBtn.addEventListener('click', throttle(onStart, 500));
 stopBtn.addEventListener('click', onStop);
 
-// const PROMT_DELAY = 1000;
 
 function onStart(evt) {
   if (!evt) {
-    return;
+       return;
   }
         console.log('Змінюємо колір!');
-        setInterval(changeColors,CHANGE_COLOR);
+        TIMER_Id = setInterval(changeColors,CHANGE_COLOR);
+        startBtn.disabled = true;
  }
     
-// // changeColors();
-// setInterval(changeColors,CHANGE_COLOR);
-// 
 
-function onStop(evt) {
-    const timerId = setInterval(changeColors,CHANGE_COLOR);
-    if(evt){
-        console.log('Зупиняємо інтервал!');
-        clearInterval(timerId);
-    }
-             
+function onStop() {
+   
+    console.log('Зупиняємо інтервал!');
+    clearInterval(TIMER_Id);  
+    startBtn.disabled = false;
+      
 
 }
 
