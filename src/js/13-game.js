d8666e5e426a3fc652bd2d41210cabaacf2a5d72
nellievropa <nellievropa23@gmail.com>
2023-11-03 15:56:47 +0200

    game work

--- src/js/13-game.js
+++ src/js/13-game.js
@@ -6,9 +6,31 @@ start.addEventListener('click', onStart);
 
 function onStart(){
     // розпилюэмо псевдомасив children, перебираємо його ForEach. на кожній ітерації отримаємо box
-    [...container.children].forEach(box => box.textContent = '@')
+    [...container.children].forEach((box, i)=> {
+        createPromise(i)
+        .then((smile)=> {
+            box.textContent = smile;
+        })
+        .catch((smile) => (box.textContent = smile))
+    })
     // в цьому випадку з'являються всі зразу
-    // а ми хочемо, щоб по черзі через секунду і різні
     
     // console.dir(container);
+}
+// а ми хочемо, щоб по черзі через секунду і різні
+
+function createPromise(delay) {
+return new Promise ((res, rej) =>{
+const random = Math.random();
+
+setTimeout(() => {
+    if(random > 0.7) {
+        res('G')
+    }else {
+        rej('F')
+    }
+}, 1000 * delay)
+
+
+});
 }
