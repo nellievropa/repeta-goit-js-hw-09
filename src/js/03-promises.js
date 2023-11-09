0f5519de87e5ac26395cd92eb11051474f6c7412
nellievropa <nellievropa23@gmail.com>
2023-11-09 17:02:55 +0200

    promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -8,87 +8,131 @@ const refs = {
   amountField: document.querySelector('.js-number'),
 }
 
-
-// let delay = 0;
-// let step = 0;
-// let position = 0;
-
-// const position = refs.amountField.value;
+let i = 0;
+// const promises = [];
 // const delay = refs.firstDelayField.value;
 // const step = refs.delayStepField.value;
-// console.log(position)
-// console.log(delay)
-// console.log(step)
-console.dir(refs.formFild);
 
-refs.createPromiseBtn.addEventListener('click', run)
+// refs.createPromiseBtn.addEventListener('click', onStart)
 
+refs.formFild.addEventListener('submit', onRun)
 
+function onRun(event) {
+  event.preventDefault()
+  const delay = refs.firstDelayField.value;
+const step = refs.delayStepField.value;
+const position = refs.amountField.value;
 
-function run(){
-  let i = 0;
- const result = [];
-  position = refs.amountField.value;
-  delay = refs.firstDelayField.value;
-  // step = refs.delayStepField.value;
-  // console.log(position)
-  // console.log(delay)
-  // console.log(step)
-  // console.dir(refs.formFild);
-  // refs.formFild.forEach((delay, position) => {
-  //   createPromise(position)
-  //   .then((prom) => {
-  //     console.log(`✅ Fulfilled promise ${position} in ${delay + step}ms`)
-  //   })
-  //   .catch((prom) => {
-  //     console.log(`❌ Rejected promise ${position} in ${delay + step}ms`)
-  //   })
-  // })
-for (i = 0; i <= position; i =+ 1) {
-  createPromise(position, delay )
-  .then(({ position, delay }) => {
-    prom.position = position;
-    prom.delay = delay + step;
-    result.push()
-
+if (delay <= 0 || step < 0 || position < 0) {
+  alert('fill fields!')
+}
+for (let i = 0; i <= position; i +=1) {
+  
+  createPromise(i, delay + step * i)
+  .then(({position, delay}) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)
   })
-  .catch(({ position, delay }) => {
-    prom.position = position;
-    prom.delay = delay + step;
-    result.push()
+  .catch(({position, delay}) => {
+    console.log(`❌ Rejected promise ${position} in ${delay + step}ms`)
+   
   })
-  .finally(() => {
-
-  })
+  event.currentTarget.reset();
 }
 }
+// function run(){
+//   let i = 0;
+//  const result = [];
+//   position = refs.amountField.value;
+//   delay = refs.firstDelayField.value;
+//   // step = refs.delayStepField.value;
+//   // console.log(position)
+//   // console.log(delay)
+//   // console.log(step)
+//   // console.dir(refs.formFild);
+//   // refs.formFild.forEach((delay, position) => {
+//   //   createPromise(position)
+//   //   .then((prom) => {
+//   //     console.log(`✅ Fulfilled promise ${position} in ${delay + step}ms`)
+//   //   })
+//   //   .catch((prom) => {
+//   //     console.log(`❌ Rejected promise ${position} in ${delay + step}ms`)
+//   //   })
+//   // })
+// for (i = 0; i <= position; i =+ 1) {
+//   createPromise(position, delay )
+//   .then(({ position, delay }) => {
+//     prom.position = position;
+//     prom.delay = delay + step;
+//     result.push()
 
+//   })
+//   .catch(({ position, delay }) => {
+//     prom.position = position;
+//     prom.delay = delay + step;
+//     result.push()
+//   })
+//   .finally(() => {
 
+//   })
+// }
+// }
 
+// function onStart() {
+//   const position = refs.amountField.value;
+ 
+//   for (i = 0; i <= position; i =+ 1){
 
+//     // promises = promises.push(createPromise(i))
+//     const promises = promises.push(promise)
+//     .then(({position, delay, step}) => {
+//       console.log(`✅ Fulfilled promise ${position} in ${delay + step}ms`)
+//     })
+//     .catch(({position, delay, step}) => {
+//       console.log(`❌ Rejected promise ${position} in ${delay + step}ms`);
+//     })
+//   }
+// }
+
+// console.log(promises)
 
 function createPromise(position, delay) {
-  const position = refs.amountField.value;
-  const delay = refs.firstDelayField.value;
-  const step = refs.delayStepField.textContent;
-  // step = refs.delayStepField.value;
   return new Promise ((res, rej) => {
     const shouldResolve = Math.random() > 0.3;
 
-// console.log(step)
-    setTimeout(() => {
-      if (shouldResolve) {
-        res(console.log(`✅ Fulfilled promise ${position} in ${delay}ms`));
-      } else {
-        rej(console.log(`❌ Rejected promise ${position} in ${delay}ms`));
-      }
-    }, delay + step )
-
-    });
-  
-
+setTimeout(() => {
+  if (shouldResolve) {
+    res({position, delay});
+  } else {
+    rej({position, delay});
+  }
+}, delay)
+  });
 
 }
+
+// function createPromise(position, delay) {
+//   const position = refs.amountField.value;
+//   const delay = refs.firstDelayField.value;
+//   const step = refs.delayStepField.textContent;
+//   // step = refs.delayStepField.value;
+//   return new Promise ((res, rej) => {
+//     const shouldResolve = Math.random() > 0.3;
+
+// // console.log(step)
+//     setTimeout(() => {
+//       if (shouldResolve) {
+//         res(console.log(`✅ Fulfilled promise ${position} in ${delay}ms`));
+//       } else {
+//         rej(console.log(`❌ Rejected promise ${position} in ${delay}ms`));
+//       }
+//     }, delay + step )
+
+//     });
+// }
+
+
+
+
 
 // createPromise(2, 1500)
 //   .then(({ position, delay }) => {
@@ -99,11 +143,12 @@ function createPromise(position, delay) {
 //   });
 
 
-// function run (amount) {
-//   const promises = new Promise((resolve, reject) => {
-//       const currentAmount = refs.amountField.value;
-//       const firstDelay = refs.firstDelayField.value;
-//       // const step = refs.delayStepField.textContent;
+// function run () {}
+
+//   const promise = new Promise((resolve, reject) => {
+//       const position= refs.amountField.value;
+//       const delay = refs.firstDelayField.value;
+//       const step = refs.delayStepField.textContent;
 
 //       // Promise.resolve('3').then(value => console.log(value))
 //       // console.log(currentAmount)
@@ -112,14 +157,16 @@ function createPromise(position, delay) {
   
 //       setTimeout(() => {
 //           // повертаємо об'єкт зі значеннями наприклад {"horse": "Mango","time": 2329 }
-//           resolve({firstDelay, currentAmount})
+//           resolve({delay, position, step})
 //           // Promise.resolve('firstDelay').then(value => console.log(value))
           
-//       }, firstDelay);
+//       }, delay);
   
 //   });
-  
-//   // };
+//   console.log(promise)
+
+
+
 // // const promises = horses.map(run)
 // promises.then(value => {console.log(value)});
 
--- src/js/13-game.js
+++ src/js/13-game.js
@@ -4,51 +4,105 @@ const container = document.querySelector('.js-container');
 
 start.addEventListener('click', onStart);
 
+// function onStart(){
+//     // створюємо масив, щоб перевірити на виграшний результата
+//     const result = [];
+//     // очищуємо кожен бокс після кожної гри 
+//     [...container.children].forEach((box) => box.textContent = '');
+//     // розпилюэмо псевдомасив children, перебираємо його ForEach. на кожній ітерації отримаємо box
+//     [...container.children].forEach((box, i)=> {
+//         createPromise(i)
+//         .then((smile)=> {
+//             box.textContent = smile;
+//             result.push('1');
+//         })
+//         .catch((smile) => (box.textContent = smile))
+//         .finally(() => {
+// setTimeout(() => {
+//     if (i === container.children.length - 1) {
+//         if(!result.length || result.length === 3) {
+//             alert('Winner!')
+//         }else{
+//             alert('Lost money!')
+//         }
+//     }
+// }, 1000)
+//         })
+//     })
+//     // в цьому випадку з'являються всі зразу
+    
+//     // console.dir(container);
+// }
+// // а ми хочемо, щоб по черзі через секунду і різні
+// // створюємо цю функцію, щоб описати тут всі її властивості!!!
+
+// function createPromise(delay) {
+// return new Promise ((res, rej) =>{
+// const random = Math.random();
+// // const first = Math.floor(Math.random()*10);
+// // const arr = ['✅','❌','😀' ]
+// setTimeout(() => {
+//     if(random > 0.7) {
+//         res('😊')
+//     }else {
+//         rej('🤑')
+//     }
+// }, 1000 * delay)
+
+
+// });
+// }
+
+// 2 ВАРІАНТ будемо робити те саме через колекцію промісів
+
 function onStart(){
     // створюємо масив, щоб перевірити на виграшний результата
-    const result = [];
-    // очищуємо кожен бокс після кожної гри 
+    let counter = 0;
+
     [...container.children].forEach((box) => box.textContent = '');
-    // розпилюэмо псевдомасив children, перебираємо його ForEach. на кожній ітерації отримаємо box
-    [...container.children].forEach((box, i)=> {
-        createPromise(i)
-        .then((smile)=> {
-            box.textContent = smile;
-            result.push('1');
-        })
-        .catch((smile) => (box.textContent = smile))
-        .finally(() => {
-setTimeout(() => {
-    if (i === container.children.length - 1) {
-        if(!result.length || result.length === 3) {
-            alert('Winner!')
-        }else{
-            alert('Lost money!')
-        }
-    }
-}, 1000)
-        })
+
+    // неактивний елемент box можна замінити на _ -нижнє підкреслення- щоб він не заважав дивитися консоль
+    const promises = [...container.children].map((_, i)=> createPromise(i));
+    Promise.allSettled(promises).then((items) => {
+        items
+        .forEach((item, i) => {
+            // console.log(item);
+            setTimeout(() => {
+                if (item.status === 'fulfilled') {
+                    counter += 1;
+                }
+            container.children[i].textContent = item.value || item.reason;
+
+           if (container.children.length - 1 === i) {
+            setTimeout(() => {
+                if (counter === container.children.length || !counter) {
+                    alert('WINNER!')
+                } else {
+                    alert('LOST MONEY!!!')
+                }
+            }, 500);
+         
+           }
+       
+        }, i * 1000);
+        });
+   
     })
-    // в цьому випадку з'являються всі зразу
     
-    // console.dir(container);
+    
+    console.log(promises);
 }
-// а ми хочемо, щоб по черзі через секунду і різні
-// створюємо цю функцію, щоб описати тут всі її властивості!!!
+
 
 function createPromise(delay) {
 return new Promise ((res, rej) =>{
 const random = Math.random();
-// const first = Math.floor(Math.random()*10);
-// const arr = ['✅','❌','😀' ]
-setTimeout(() => {
-    if(random > 0.7) {
-        res('😊')
-    }else {
-        rej('🤑')
-    }
-}, 1000 * delay)
 
+    if(random > 0.5) {
+        res('😊');
+    }else {
+        rej('🤑');
+    }
 
 });
 }
