68825a33a9e9e2509898cf68a8aed68cc244442e
nellievropa <nellievropa23@gmail.com>
2023-11-10 14:24:48 +0200

    final

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -8,7 +8,7 @@ const refs = {
   amountField: document.querySelector('.js-number'),
 }
 
-let i = 0;
+// let i = 0;
 // const promises = [];
 // const delay = refs.firstDelayField.value;
 // const step = refs.delayStepField.value;
@@ -18,82 +18,29 @@ let i = 0;
 refs.formFild.addEventListener('submit', onRun)
 
 function onRun(event) {
-  event.preventDefault()
-  const delay = refs.firstDelayField.value;
-const step = refs.delayStepField.value;
-const position = refs.amountField.value;
+ event.preventDefault()
+let delay = Number(refs.firstDelayField.value);
+let step = Number(refs.delayStepField.value);
+const position = Number(refs.amountField.value);
+console.log(position)
 
 if (delay <= 0 || step < 0 || position < 0) {
-  alert('fill fields!')
+  Notify.warning('Fulfilled fields!')
 }
-for (let i = 0; i <= position; i +=1) {
-  
-  createPromise(i, delay + step * i)
+for (let i = 0; i <= position - 1; i += 1) {
+  delay += step;
+  createPromise(i, delay)
   .then(({position, delay}) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)
+    Notify.success(`✅ Fulfilled promise ${position + 1} in ${delay}ms`);
+    
   })
   .catch(({position, delay}) => {
-    console.log(`❌ Rejected promise ${position} in ${delay + step}ms`)
-   
+    Notify.failure(`❌ Rejected promise ${position + 1} in ${delay}ms`)
+    
   })
-  event.currentTarget.reset();
-}
-}
-// function run(){
-//   let i = 0;
-//  const result = [];
-//   position = refs.amountField.value;
-//   delay = refs.firstDelayField.value;
-//   // step = refs.delayStepField.value;
-//   // console.log(position)
-//   // console.log(delay)
-//   // console.log(step)
-//   // console.dir(refs.formFild);
-//   // refs.formFild.forEach((delay, position) => {
-//   //   createPromise(position)
-//   //   .then((prom) => {
-//   //     console.log(`✅ Fulfilled promise ${position} in ${delay + step}ms`)
-//   //   })
-//   //   .catch((prom) => {
-//   //     console.log(`❌ Rejected promise ${position} in ${delay + step}ms`)
-//   //   })
-//   // })
-// for (i = 0; i <= position; i =+ 1) {
-//   createPromise(position, delay )
-//   .then(({ position, delay }) => {
-//     prom.position = position;
-//     prom.delay = delay + step;
-//     result.push()
-
-//   })
-//   .catch(({ position, delay }) => {
-//     prom.position = position;
-//     prom.delay = delay + step;
-//     result.push()
-//   })
-//   .finally(() => {
-
-//   })
-// }
-// }
-
-// function onStart() {
-//   const position = refs.amountField.value;
- 
-//   for (i = 0; i <= position; i =+ 1){
-
-//     // promises = promises.push(createPromise(i))
-//     const promises = promises.push(promise)
-//     .then(({position, delay, step}) => {
-//       console.log(`✅ Fulfilled promise ${position} in ${delay + step}ms`)
-//     })
-//     .catch(({position, delay, step}) => {
-//       console.log(`❌ Rejected promise ${position} in ${delay + step}ms`);
-//     })
-//   }
-// }
-
-// console.log(promises)
+ }
+ event.currentTarget.reset();
+};
 
 function createPromise(position, delay) {
   return new Promise ((res, rej) => {
@@ -109,66 +56,6 @@ setTimeout(() => {
   });
 
 }
-
-// function createPromise(position, delay) {
-//   const position = refs.amountField.value;
-//   const delay = refs.firstDelayField.value;
-//   const step = refs.delayStepField.textContent;
-//   // step = refs.delayStepField.value;
-//   return new Promise ((res, rej) => {
-//     const shouldResolve = Math.random() > 0.3;
-
-// // console.log(step)
-//     setTimeout(() => {
-//       if (shouldResolve) {
-//         res(console.log(`✅ Fulfilled promise ${position} in ${delay}ms`));
-//       } else {
-//         rej(console.log(`❌ Rejected promise ${position} in ${delay}ms`));
-//       }
-//     }, delay + step )
-
-//     });
-// }
-
-
-
-
-
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
-
-
-// function run () {}
-
-//   const promise = new Promise((resolve, reject) => {
-//       const position= refs.amountField.value;
-//       const delay = refs.firstDelayField.value;
-//       const step = refs.delayStepField.textContent;
-
-//       // Promise.resolve('3').then(value => console.log(value))
-//       // console.log(currentAmount)
-//       // console.log(firstDelay)
-//       // console.log(step)
-  
-//       setTimeout(() => {
-//           // повертаємо об'єкт зі значеннями наприклад {"horse": "Mango","time": 2329 }
-//           resolve({delay, position, step})
-//           // Promise.resolve('firstDelay').then(value => console.log(value))
-          
-//       }, delay);
-  
-//   });
-//   console.log(promise)
-
-
-
-// // const promises = horses.map(run)
-// promises.then(value => {console.log(value)});
 
 
 
