e67db4f59dec361420e3c6079192f740ca466ab1
nellievropa <124687833+nellievropa@users.noreply.github.com>
2023-11-29 09:37:32 +0000

    Deploying to gh-pages from @ nellievropa/repeta-goit-js-hw-09@1785ec7ebbea741d62b50e5345f0782a97f9a133 🚀

--- 05-date.14f7c5b4.js
+++
@@ -1,2 +0,0 @@
-const o=new Date;console.log(o),setTimeout((()=>{const e=(new Date).getTime();console.log(o),console.log(e),console.log(e-o)}),3e3);const e=Date.now();console.log("date3",e);
-//# sourceMappingURL=05-date.14f7c5b4.js.map
